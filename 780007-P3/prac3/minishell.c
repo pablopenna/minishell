a79cc8418454c2c60e4ba7102f7e31c51a175423
Pablo Pena <pablo@debian>
2015-03-27 20:37:44 +0100

    EVERYTHING finished (probably)

--- 780007-P3/prac3/minishell.c
+++ 780007-P3/prac3/minishell.c
@@ -18,26 +18,55 @@
 int main (int argc, char *argv[])
 {
 	char buf[BUFSIZ];
-	
 	while(1)
 	{
 		print_prompt();
 		read_command_line(buf);
 
-		if(strcmp(buf,"exit")==0){
-			jobs_free_mem();
-			exit(0);
-		}
-
-		else
-		{
-			if(is_internal_command(buf))//internal command
-			{
-				execute_internal_command(buf);//executed internal command
+		if(strchr(buf,';')==NULL)//si no hay ';' en buf->orden simple
+		{						//strchr busca ; en buf devolviendo NULL si no  hay
+			if(strcmp(buf,"exit")==0){
+				jobs_free_mem();
+				exit(0);
 			}
+
 			else
 			{
-				execute_external_command(buf);
+				if(is_internal_command(buf))//internal command
+				{
+					execute_internal_command(buf);//executed internal command
+				}
+				else
+				{
+					execute_external_command(buf);
+				}
+			}
+		}
+			
+		else//si hay ';' en buf->orden compuesta
+		{
+			
+			char * buf_split;
+			buf_split=strtok(buf,";");
+			while(buf_split!=NULL)
+			{
+				if(strcmp(buf_split,"exit")==0){
+					jobs_free_mem();
+					exit(0);
+				}
+
+				else
+				{
+					if(is_internal_command(buf_split))//internal command
+					{
+						execute_internal_command(buf_split);//execute internal command
+					}
+					else
+					{
+						execute_external_command(buf_split);
+					}
+				}
+			buf_split=strtok(NULL,";");
 			}
 		}
 	}
